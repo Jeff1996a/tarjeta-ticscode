779bf12192094037bcd48fa87e4970d7bebcc3c9
Jeff1996a <jefferson.alfaro@epn.edu.ec>
2023-09-17 22:06:39 -0500

    sss

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg  color="#7fa961" num={60} type="cobweb" bg={true} className="particles-bg-canvas" />
+        <ParticlesBg  color="#7fa961" num={60} type="cobweb" bg={true}/>
 
         <div className="row banner">
           <div className="banner-text">
