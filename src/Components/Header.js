85cf9276bf81503b47d4364a5fae941b5ede5b2c
Jeff1996a <jefferson.alfaro@epn.edu.ec>
2023-09-18 00:04:55 -0500

    update homepage

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,23 +19,23 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <Fade top>
+            <Fade bottom>
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
-            <Fade top>
+            <Fade bottom>
               <div className="responsive-headline">
                 <Zmage src="./images/perfil.jpg"  alt="" className="perfil"/>
               </div>
             </Fade>
             
-            <Fade top>
+            <Fade bottom>
               <h2 className="responsive-headline">{profession}</h2>
             </Fade>
             <Fade top duration={1200}>
               <h3>{description}.</h3>
             </Fade>
             <hr />
-            <Fade top duration={2000}>
+            <Fade right duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
                   <i className="fa fa-book"></i>Proyectos
